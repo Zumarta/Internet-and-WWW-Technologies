30d303c275c2f3242dc5b37a006878c52a685571
Lorenz Woth <lorenz.woth@outlook.com>
2019-05-04 10:10:07 +0200

    Implemented CRC checksum verification

--- exercise3/04-01.py
+++ exercise3/04-01.py
@@ -1,23 +1,22 @@
 import argparse
+import re
 
-# Helpful: https://de.wikipedia.org/wiki/Zyklische_Redundanzpr%C3%BCfung
+
 def main():
-    # FIXME
-    # bitstring, polynomial = read_arguments()
+    # Get arguments
+    bitstring, polynomial = read_arguments()
 
-    # Debug
-    bitstring, polynomial = [1011010101110011], [1011]
+    # Perform CRC verification
+    result = crc_check(bitstring, polynomial)
 
-    # TODO: outsource
-    poly_degree = len(polynomial)
-
-    # Fill bitstring with zeroes based on the polynomial degree
-    for _ in polynomial:
-        bitstring.append(0)
+    if result == 0:
+        exit(0)
+    else:
+        exit(25)
 
 
 def read_arguments():
-    """ Return bitstring and polynomial in its string format """
+    """ Return bitstring and polynomial in its string format. """
     parser = argparse.ArgumentParser(description='Sum or multiply integers.')
 
     parser.add_argument('bitstring', metavar='N', nargs='+', type=int, help='Bitstring')
@@ -25,7 +24,50 @@ def read_arguments():
 
     args = parser.parse_args()
 
-    return args.bitstring, args.polynomial
+    # Parse lists to strings and remove brackets
+    bitstring = str(args.bitstring).strip("[]")
+    polynomial = str(args.polynomial).strip("[]")
+
+    return bitstring, polynomial
+
+
+def crc_check(bitstring, polynomial):
+    """ Return 0 if the checksum was correct, otherwise return 25 """
+    # RegEx pattern for a zero-check
+    only_zeroes_pattern = re.compile("^[0]+$")
+
+    rest = ""
+    while len(bitstring) >= len(polynomial):
+        i = 0
+        while i < len(polynomial):
+            if set(bitstring[i]) ^ set(polynomial[i]):
+                rest += "1"
+            else:
+                rest += "0"
+
+            i += 1
+
+        # If our rest contains only zeroes and the bitstring length is equal to polynomial length, check was successful
+        if only_zeroes_pattern.match(rest) and (len(bitstring) <= len(polynomial)):
+            return 0
+
+        # Reuse bitstring and cut off already checked bits
+        bitstring = bitstring[len(polynomial):]
+
+        # Delete leading zeroes
+        rest = rest.lstrip("0")
+
+        # Add not used part of bitstring to rest
+        bitstring = rest + bitstring
+
+        # Reset rest then
+        rest = ""
+
+    """ 
+    If we arrive here, there has been a non-zero rest in our calculation and that said, 
+    the message has not been delivered successfully 
+    """
+    return 25
 
 
 if __name__ == '__main__':
