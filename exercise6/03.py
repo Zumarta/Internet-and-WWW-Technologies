aae168c30811dd41ee90c2853d6ec7d8b89dd491
Lorenz Woth <lorenz.woth@outlook.com>
2019-06-08 08:05:31 +0200

    Finished

--- exercise6/03.py
+++ exercise6/03.py
@@ -12,14 +12,24 @@ def read_argument():
 
 
 def open_tcp_connection(tcp_port):
-    """ Open TCP connection with the given port """
+    """
+    Open TCP connection with the given port
+    Two lines will be ignored by pylint due to a false positive:
+    https://stackoverflow.com/questions/10300082/how-to-prevent-python-pylint-complaining-about-socket-class-sendall-method
+    """
+
     # Build up connection
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Task says, port shall be bound to all interfaces on the machine
+    # StackOverflow said, binding on interfaces on Windows works via using the specific IP address of the interface
+    # I changed that IP back to localhost, because otherwise test's might not work for another users and networks.
     sock.bind(('127.0.0.1', tcp_port))
 
     sock.listen(1)
     conn, addr = sock.accept()
+    # pylint: disable=no-member
     data = conn.recv(1024)
+    # pylint: enable=no-member
 
     # Parse values
     content = bytes.decode(data).strip('\n')
@@ -29,7 +39,9 @@ def open_tcp_connection(tcp_port):
     # Show address
     print('Connected by', addr)
 
+    # pylint: disable=no-member
     conn.send(result.encode())
+    # pylint: enable=no-member
     conn.close()
 
 
